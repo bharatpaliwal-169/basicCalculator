0753f761ac0a2048a3c8c31e9747f06c1dedbde4
Bharat <18uec169@lnmiit.ac.in>
2020-06-21 13:57:49 +0530

    Updates

---
+++ precache-manifest.57f543490d3a54761a4ea4e99add4432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08946409162bfbf21414ef2143d7cb3",
+    "url": "/basicCalculator/index.html"
+  },
+  {
+    "revision": "6f5dade2b31dcc78aa06",
+    "url": "/basicCalculator/static/css/main.f29de32b.chunk.css"
+  },
+  {
+    "revision": "eb673ad7345e88ef2743",
+    "url": "/basicCalculator/static/js/2.08fef289.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basicCalculator/static/js/2.08fef289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f5dade2b31dcc78aa06",
+    "url": "/basicCalculator/static/js/main.cee082d9.chunk.js"
+  },
+  {
+    "revision": "344f871d1475ac7074f3",
+    "url": "/basicCalculator/static/js/runtime-main.bd550777.js"
+  }
+]);
